eb430615882a1106078aa3b7548f86c7fc3bf220
pan93412 <pan93412@gmail.com>
2022-04-04 02:09:52 +0800

    feat: compress & readme

--- build.fish
+++ build.fish
@@ -65,9 +65,44 @@ mshta vbscript:createobject("wscript.shell").run("""%~nx0"" h",0)(window.close)&
 "%~dp0"\app\YesPlayMusic.exe' > $ypm_app_dir/../start_silently.bat
 end
 
+function _hash
+    shasum -a 512 $argv | awk '{print $1}'
+end
+
+function _get_hash_txt
+    set filepath $argv[1]
+    set filename (basename $filepath)
+
+    echo $filename 的 SHA-256 hash： (_hash $filepath)
+end
+
+function create_readme -d "產生 README 檔案"
+    set ypm_src $argv[1]
+
+    echo "本壓縮包是從 $ypm_src 安裝包組建的，修正 #1145 問題的版本。" > $ypm_app_dir/../README.txt
+    echo "" >> $ypm_app_dir/../README.txt
+
+    echo (_get_hash_txt $ypm_app_dir/YesPlayMusic.exe) >> $ypm_app_dir/../README.txt
+    echo (_get_hash_txt $ypm_app_dir/../start.bat) >> $ypm_app_dir/../README.txt
+    echo (_get_hash_txt $ypm_app_dir/../start_silently.bat) >> $ypm_app_dir/../README.txt
+end
+
+function compress_artifact -d "壓縮建立完成的檔案"
+    pushd dist
+    set filename {$YPM_PORTABLE_FILE_PATH}"_WINPATCH.zip"
+
+    7z a $filename .
+
+    popd
+end
+
 create_dist
 extract_ypm $YPM_PORTABLE_FILE_PATH
 create_scripts
+create_readme $YPM_PORTABLE_FILE_PATH
+compress_artifact
+
+info "檔案全部都在 dist 資料夾。"
 
 # GC: 清理用完的目錄
 for dir in $tmp_dirs
