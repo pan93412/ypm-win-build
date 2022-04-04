d178d0f028301553ba1c11e5aa5f53e21b6e8ffc
pan93412 <pan93412@gmail.com>
2022-04-04 12:10:54 +0800

    refactor(log): output to stderr

--- log.fish
+++ log.fish
@@ -1,17 +1,17 @@
 function debug
-    echo -e "[YPM:DEBUG] $argv"
+    echo -e "[YPM:DEBUG] $argv" >&2
 end
 
 function info
-    echo -e "\x1b[1m[YPM:INFO] $argv\x1b[0m"
+    echo -e "\x1b[1m[YPM:INFO] $argv\x1b[0m" >&2
 end
 
 function warn
-    echo -e "\x1b[1;33m[YPM:WARN] $argv\x1b[0m"
+    echo -e "\x1b[1;33m[YPM:WARN] $argv\x1b[0m" >&2
 end
 
 function error
-    echo -e "\x1b[1;31m[YPM:ERROR] $argv\x1b[0m"
+    echo -e "\x1b[1;31m[YPM:ERROR] $argv\x1b[0m" >&2
 end
 
 function panic
